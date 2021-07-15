0db0b57622b7fe753f09f181df8b7e38340609a8
vishalw-iitk <vishalwghmre88@gmail.com>
2021-07-16 00:36:42 +0530

    edit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-Select from here(https://pytorch.org/) the command for installing pytorch based on the CUDA version you have.
+Select from here(https://pytorch.org/) the command for installing pytorch based on the CUDA version you have and put the command inside 'gpu_rep.py' under the variable 'CUDA_compatible_command'
+
+Uncomment skip = True inside 'gpu_req.py' to installing the dependencies for GPU for the first time
+Uncomment skip = False inside 'gpu_req.py' to stop re-installing the dependencies for GPU
+
+On Linux terminal :
+    CPU Run : bash main_Cpu.sh
+    GPU Run : bash main_Gpu.sh
+
+
--- main_cpu.sh
+++ main_cpu.sh
@@ -1,5 +1,5 @@
-# python begin.py
-# python cpu_requirements.py
-# python data_prep.py
+python begin.py
+python cpu_requirements.py
+python data_prep.py
 python yolov5/train.py --img 416 --batch 32 --epochs 5 --data 'data.yaml' --cfg yolov5/models/yolov5s.yaml --weights '' --name yolov5s_results  --cache
-# python yolov5/detect.py --source ARRANGED_DATASET_TEST/images/test/ --weights runs/train/yolov5s_results/weights/best.pt --conf-thres 0.015 --iou-thres 0.3
+python yolov5/detect.py --source ARRANGED_DATASET_TEST/images/test/ --weights runs/train/yolov5s_results/weights/best.pt --conf-thres 0.015 --iou-thres 0.3
