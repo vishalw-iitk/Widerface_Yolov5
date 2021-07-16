f122b90f77d43700667c5d1c3c99b5b89e375a25
vishalw-iitk <vishalwghmre88@gmail.com>
2021-07-16 10:36:56 +0530

    --hyp arg added

--- main_cpu.sh
+++ main_cpu.sh
@@ -1,5 +1,5 @@
 python begin.py
 python cpu_req.py
 python data_prep.py
-# python yolov5/train.py --img 416 --batch 32 --epochs 2 --data 'data_short.yaml' --cfg yolov5/models/yolov5s.yaml --weights '' --name yolov5s_results  --cache
-# python yolov5/detect.py --source ARRANGED_DATASET_TEST/images/test/ --weights runs/train/yolov5s_results/weights/best.pt --conf-thres 0.015 --iou-thres 0.3
+python ../yolov5/train.py --img 416 --batch 32 --epochs 2 --data 'data.yaml' --cfg ../yolov5/models/yolov5s.yaml --hyp ../yolov5/data/hyps/hyp.scratch.yaml --weights '' --name yolov5s_results  --cache
+python ../yolov5/detect.py --source ../ARRANGED_DATASET_TEST/images/test/ --weights ../runs/train/yolov5s_results/weights/best.pt --conf-thres 0.015 --iou-thres 0.3
