85c66e3de2e7e60d750a54fd52848b3b169a7dbb
vishalw-iitk <vishalwghmre88@gmail.com>
2021-07-16 16:23:02 +0530

    batch-size=4

--- main_gpu.sh
+++ main_gpu.sh
@@ -1,5 +1,5 @@
 python begin.py
 python gpu_req.py
 python data_prep.py
-python ../yolov5/train.py --img 416 --batch 32 --epochs 2 --data 'data.yaml' --cfg ../yolov5/models/yolov5s.yaml --hyp ../yolov5/data/hyps/hyp.scratch.yaml --project ../runs/train --weights '' --name yolov5s_results  --cache
+python ../yolov5/train.py --img 416 --batch 4 --epochs 2 --data 'data.yaml' --cfg ../yolov5/models/yolov5s.yaml --hyp ../yolov5/data/hyps/hyp.scratch.yaml --project ../runs/train --weights '' --name yolov5s_results  --cache
 python ../yolov5/detect.py --source ../ARRANGED_DATASET_TEST/images/test/ --weights ../runs/train/yolov5s_results/weights/best.pt --project ../runs/detect --conf-thres 0.015 --iou-thres 0.3
