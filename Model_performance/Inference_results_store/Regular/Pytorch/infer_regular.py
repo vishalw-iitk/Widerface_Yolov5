0779d992eee36da24baca94647ca19f0215ba1b7
vishalw-iitk <vishalwghmre88@gmail.com>
2021-08-14 16:16:25 +0530

    no map location

--- Model_performance/Inference_results_store/Regular/Pytorch/infer_regular.py
+++ Model_performance/Inference_results_store/Regular/Pytorch/infer_regular.py
@@ -40,7 +40,7 @@ def run(
 
     # model = quantized_load(weights, cfg, device, img_size, data, hyp, single_cls)
 
-    ckpt = torch.load(weights, map_location=torch.device(device))
+    ckpt = torch.load(weights)
     fitness_score = ckpt['best_fitness'] if ckpt.get('best_fitness') else None
 
     with open(data) as f:
