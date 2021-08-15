f8a21b02dd0575987307449d119bf128327ae500
vishalw-iitk <vishalwghmre88@gmail.com>
2021-08-15 22:46:19 +0530

    model fuse in QAT

--- Model_compression/Quantization/Pytorch/QAT/yolov5_repo/train.py
+++ Model_compression/Quantization/Pytorch/QAT/yolov5_repo/train.py
@@ -130,7 +130,7 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
             cfg = os.path.join(cfg),
             imgsz = 416,
             data = os.path.join(data),
-            hyp = os.path.join(hyp),
+            hyp = os.path.join(opt.hyp),
             single_cls = False,
             model_class = model_type
         )
