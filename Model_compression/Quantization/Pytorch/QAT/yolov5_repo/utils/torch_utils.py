a3fc20a58af96ba137ed3c7b4294eef1b6710c4d
vishalw-iitk <vishalwghmre88@gmail.com>
2021-08-16 17:24:56 +0530

    model fuse in QAT

--- Model_compression/Quantization/Pytorch/QAT/yolov5_repo/utils/torch_utils.py
+++ Model_compression/Quantization/Pytorch/QAT/yolov5_repo/utils/torch_utils.py
@@ -210,6 +210,8 @@ def fuse_conv_and_bn(conv, bn):
     b_conv = torch.zeros(conv.weight.size(0), device=conv.weight.device) if conv.bias is None else conv.bias
     b_bn = bn.bias - bn.weight.mul(bn.running_mean).div(torch.sqrt(bn.running_var + bn.eps))
     fusedconv.bias.copy_(torch.mm(w_bn, b_conv.reshape(-1, 1)).reshape(-1) + b_bn)
+    
+    fusedconv = fusedconv.requires_grad_(True)
 
     return fusedconv
 
