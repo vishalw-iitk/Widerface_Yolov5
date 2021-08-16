96c0302c7f9ac72bde683b15c70e1d368b193deb
vishalw-iitk <vishalwghmre88@gmail.com>
2021-08-16 21:59:31 +0530

    graph correction attempt

--- Model_compression/Quantization/Pytorch/QAT/yolov5_repo/models/yolo.py
+++ Model_compression/Quantization/Pytorch/QAT/yolov5_repo/models/yolo.py
@@ -57,7 +57,7 @@ class Detect(nn.Module):
         z = []  # inference output
         for i in range(self.nl):
             x[i] = self.m[i](x[i])  # conv
-            # x[i] = self.dequant(x[i])
+            x[i] = self.dequant(x[i])
             bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
             x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
 
