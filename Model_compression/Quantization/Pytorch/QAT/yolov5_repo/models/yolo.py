b9ebf17cb11989ee5d7ddfb0e9c72fd0ff8bb8c2
vishalw-iitk <vishalwghmre88@gmail.com>
2021-08-16 21:54:33 +0530

    graph correction attempt

--- Model_compression/Quantization/Pytorch/QAT/yolov5_repo/models/yolo.py
+++ Model_compression/Quantization/Pytorch/QAT/yolov5_repo/models/yolo.py
@@ -51,11 +51,13 @@ class Detect(nn.Module):
 
     # edited
     def forward(self, x):
+        print("number of layers", self.nl)
+        print("is training", self.training)
         # x = x.copy()  # for profiling
         z = []  # inference output
         for i in range(self.nl):
             x[i] = self.m[i](x[i])  # conv
-            x[i] = self.dequant(x[i])
+            # x[i] = self.dequant(x[i])
             bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
             x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
 
@@ -65,7 +67,7 @@ class Detect(nn.Module):
 
                 y = x[i].sigmoid()
                 if self.inplace:
-                    y[..., 0:2] = (y[..., 0:2] * 2.  + self.grid[i]) * self.stride[i]  # xy
+                    y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
                     y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
                 else:  # for YOLOv5 on AWS Inferentia https://github.com/ultralytics/yolov5/pull/2953
                     xy = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
@@ -73,14 +75,8 @@ class Detect(nn.Module):
                     y = torch.cat((xy, wh, y[..., 4:]), -1)
                 z.append(y.view(bs, -1, self.no))
 
-        if self.training:
-            # print("train mode")
-            return x
-        else:
-            ct = torch.cat(z, 1)
-            # print("evaluation mode")
-            return (ct, x)
-        
+        return x if self.training else (torch.cat(z, 1), x)
+     
 
     @staticmethod
     def _make_grid(nx=20, ny=20):
