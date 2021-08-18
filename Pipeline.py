acfafd6fc2081131f1c6eef0994eb753535237f2
vishalw-iitk <vishalwghmre88@gmail.com>
2021-08-18 19:49:17 +0530

    sr to 0.1

--- Pipeline.py
+++ Pipeline.py
@@ -119,7 +119,7 @@ def main(opt):
         project = train_results_paths['Pruning']['Pytorch']['P1'],
         name = model_names['Pruning']['Pytorch']['P1'],
         st = True,
-        sr = 0.001
+        sr = 0.1
         )
 
     quantization.run(       #Quantization.py
