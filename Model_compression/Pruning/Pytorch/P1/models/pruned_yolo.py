6ce2c73dda21c3fd6f6cc5031a9a4a7dfee06407
vishalw-iitk <vishalwghmre88@gmail.com>
2021-08-19 13:06:24 +0530

    pruning model ModelPruned edit

--- Model_compression/Pruning/Pytorch/P1/models/pruned_yolo.py
+++ Model_compression/Pruning/Pytorch/P1/models/pruned_yolo.py
@@ -14,7 +14,7 @@ from dts.Model_compression.Pruning.Pytorch.P1.models.pruned_common import *
 from yolov5.models.experimental import *
 from yolov5.utils.autoanchor import check_anchor_order
 from yolov5.utils.general import make_divisible, check_file, set_logging
-from yolov5.utils.torch_utils import time_synchronized, fuse_conv_and_bn, model_info, scale_img, initialize_weights, \
+from yolov5.utils.torch_utils import fuse_conv_and_bn, model_info, scale_img, initialize_weights, \
     select_device, copy_attr
 
 try:
