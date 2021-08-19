564b7e48179afb02b529baf17f58eae90b7f8097
vishalw-iitk <vishalwghmre88@gmail.com>
2021-08-19 13:16:45 +0530

    pruned yolo.py

--- Model_compression/Pruning/Pytorch/P1/models/pruned_yolo.py
+++ Model_compression/Pruning/Pytorch/P1/models/pruned_yolo.py
@@ -241,6 +241,7 @@ class ModelPruned(nn.Module):
 
         # Define model
         ch = self.yaml['ch'] = self.yaml.get('ch', ch)  # input channels
+        print(ch)
         if nc and nc != self.yaml['nc']:
             logger.info(f"Overriding model.yaml nc={self.yaml['nc']} with nc={nc}")
             self.yaml['nc'] = nc  # override yaml value
@@ -387,58 +388,58 @@ class ModelPruned(nn.Module):
 
 
 
-def parse_model(d, ch):  # model_dict, input_channels(3)
-    logger.info('\n%3s%18s%3s%10s  %-40s%-30s' % ('', 'from', 'n', 'params', 'module', 'arguments'))
-    anchors, nc, gd, gw = d['anchors'], d['nc'], d['depth_multiple'], d['width_multiple']
-    na = (len(anchors[0]) // 2) if isinstance(anchors, list) else anchors  # number of anchors
-    no = na * (nc + 5)  # number of outputs = anchors * (classes + 5)
-    print("Now parse model ... ")
-    layers, save, c2 = [], [], ch[-1]  # layers, savelist, ch out
-    for i, (f, n, m, args) in enumerate(d['backbone'] + d['head']):  # from, number, module, args
-        m = eval(m) if isinstance(m, str) else m  # eval strings
-        for j, a in enumerate(args):
-            try:
-                args[j] = eval(a) if isinstance(a, str) else a  # eval strings
-            except:
-                pass
+# def parse_model(d, ch):  # model_dict, input_channels(3)
+#     logger.info('\n%3s%18s%3s%10s  %-40s%-30s' % ('', 'from', 'n', 'params', 'module', 'arguments'))
+#     anchors, nc, gd, gw = d['anchors'], d['nc'], d['depth_multiple'], d['width_multiple']
+#     na = (len(anchors[0]) // 2) if isinstance(anchors, list) else anchors  # number of anchors
+#     no = na * (nc + 5)  # number of outputs = anchors * (classes + 5)
+#     print("Now parse model ... ")
+#     layers, save, c2 = [], [], ch[-1]  # layers, savelist, ch out
+#     for i, (f, n, m, args) in enumerate(d['backbone'] + d['head']):  # from, number, module, args
+#         m = eval(m) if isinstance(m, str) else m  # eval strings
+#         for j, a in enumerate(args):
+#             try:
+#                 args[j] = eval(a) if isinstance(a, str) else a  # eval strings
+#             except:
+#                 pass
 
-        n = max(round(n * gd), 1) if n > 1 else n  # depth gain
-        if m in [Conv, GhostConv, Bottleneck, GhostBottleneck, SPP, DWConv, MixConv2d, Focus, CrossConv, BottleneckCSP,
-                 C3, C3TR]:
-            c1, c2 = ch[f], args[0]
-            if c2 != no:  # if not output
-                c2 = make_divisible(c2 * gw, 8)
+#         n = max(round(n * gd), 1) if n > 1 else n  # depth gain
+#         if m in [Conv, GhostConv, Bottleneck, GhostBottleneck, SPP, DWConv, MixConv2d, Focus, CrossConv, BottleneckCSP,
+#                  C3, C3TR]:
+#             c1, c2 = ch[f], args[0]
+#             if c2 != no:  # if not output
+#                 c2 = make_divisible(c2 * gw, 8)
 
-            args = [c1, c2, *args[1:]]
-            if m in [BottleneckCSP, C3, C3TR]:
-                args.insert(2, n)  # number of repeats
-                n = 1
-        elif m is nn.BatchNorm2d:
-            args = [ch[f]]
-        elif m is Concat:
-            c2 = sum([ch[x] for x in f])
-        elif m is Detect:
-            args.append([ch[x] for x in f])
-            if isinstance(args[1], int):  # number of anchors
-                args[1] = [list(range(args[1] * 2))] * len(f)
-        elif m is Contract:
-            c2 = ch[f] * args[0] ** 2
-        elif m is Expand:
-            c2 = ch[f] // args[0] ** 2
-        else:
-            c2 = ch[f]
-        print("N : ", n)
-        m_ = nn.Sequential(*[m(*args) for _ in range(n)]) if n > 1 else m(*args)  # module
-        t = str(m)[8:-2].replace('__main__.', '')  # module type
-        np = sum([x.numel() for x in m_.parameters()])  # number params
-        m_.i, m_.f, m_.type, m_.np = i, f, t, np  # attach index, 'from' index, type, number params
-        logger.info('%3s%18s%3s%10.0f  %-40s%-30s' % (i, f, n, np, t, args))  # print
-        save.extend(x % i for x in ([f] if isinstance(f, int) else f) if x != -1)  # append to savelist
-        layers.append(m_)
-        if i == 0:
-            ch = []
-        ch.append(c2)
-    return nn.Sequential(*layers), sorted(save)
+#             args = [c1, c2, *args[1:]]
+#             if m in [BottleneckCSP, C3, C3TR]:
+#                 args.insert(2, n)  # number of repeats
+#                 n = 1
+#         elif m is nn.BatchNorm2d:
+#             args = [ch[f]]
+#         elif m is Concat:
+#             c2 = sum([ch[x] for x in f])
+#         elif m is Detect:
+#             args.append([ch[x] for x in f])
+#             if isinstance(args[1], int):  # number of anchors
+#                 args[1] = [list(range(args[1] * 2))] * len(f)
+#         elif m is Contract:
+#             c2 = ch[f] * args[0] ** 2
+#         elif m is Expand:
+#             c2 = ch[f] // args[0] ** 2
+#         else:
+#             c2 = ch[f]
+#         print("N : ", n)
+#         m_ = nn.Sequential(*[m(*args) for _ in range(n)]) if n > 1 else m(*args)  # module
+#         t = str(m)[8:-2].replace('__main__.', '')  # module type
+#         np = sum([x.numel() for x in m_.parameters()])  # number params
+#         m_.i, m_.f, m_.type, m_.np = i, f, t, np  # attach index, 'from' index, type, number params
+#         logger.info('%3s%18s%3s%10.0f  %-40s%-30s' % (i, f, n, np, t, args))  # print
+#         save.extend(x % i for x in ([f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+#         layers.append(m_)
+#         if i == 0:
+#             ch = []
+#         ch.append(c2)
+#     return nn.Sequential(*layers), sorted(save)
 
 
 def parse_pruned_model(maskbndict, d, ch):  # model_dict, input_channels(3)
@@ -453,6 +454,7 @@ def parse_pruned_model(maskbndict, d, ch):  # model_dict, input_channels(3)
     layers, save, c2 = [], [], ch[-1]  # layers, savelist, ch out
     # layertmp = fromlayer[-1]
     for i, (f, n, m, args) in enumerate(d['backbone'] + d['head']):  # from, number, module, args
+        print(f)
         m = eval(m) if isinstance(m, str) else m  # eval strings
         for j, a in enumerate(args):
             try:
@@ -547,6 +549,7 @@ def parse_pruned_model(maskbndict, d, ch):  # model_dict, input_channels(3)
         elif m is Expand:
             c2 = ch[f] // args[0] ** 2
         else:
+
             c2 = ch[f]
             fromtmp = fromlayer[-1]
             fromlayer.append(fromtmp)
