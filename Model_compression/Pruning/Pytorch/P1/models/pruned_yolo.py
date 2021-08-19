1e4c64fe4e139a3634ba005d8d63818d0ac1ef41
vishalw-iitk <vishalwghmre88@gmail.com>
2021-08-19 13:07:52 +0530

    pruned yolo.py

--- Model_compression/Pruning/Pytorch/P1/models/pruned_yolo.py
+++ Model_compression/Pruning/Pytorch/P1/models/pruned_yolo.py
@@ -23,51 +23,51 @@ except ImportError:
     thop = None
 
 
-# class Detect(nn.Module):
-#     stride = None  # strides computed during build
-#     onnx_dynamic = False  # ONNX export parameter
+class Detect(nn.Module):
+    stride = None  # strides computed during build
+    onnx_dynamic = False  # ONNX export parameter
 
-#     def __init__(self, nc=80, anchors=(), ch=(), inplace=True):  # detection layer
-#         super(Detect, self).__init__()
-#         self.nc = nc  # number of classes
-#         self.no = nc + 5  # number of outputs per anchor
-#         self.nl = len(anchors)  # number of detection layers
-#         self.na = len(anchors[0]) // 2  # number of anchors
-#         self.grid = [torch.zeros(1)] * self.nl  # init grid
-#         a = torch.tensor(anchors).float().view(self.nl, -1, 2)
-#         self.register_buffer('anchors', a)  # shape(nl,na,2)
-#         self.register_buffer('anchor_grid', a.clone().view(self.nl, 1, -1, 1, 1, 2))  # shape(nl,1,na,1,1,2)
-#         self.m = nn.ModuleList(nn.Conv2d(x, self.no * self.na, 1) for x in ch)  # output conv
-#         self.inplace = inplace  # use in-place ops (e.g. slice assignment)
+    def __init__(self, nc=80, anchors=(), ch=(), inplace=True):  # detection layer
+        super(Detect, self).__init__()
+        self.nc = nc  # number of classes
+        self.no = nc + 5  # number of outputs per anchor
+        self.nl = len(anchors)  # number of detection layers
+        self.na = len(anchors[0]) // 2  # number of anchors
+        self.grid = [torch.zeros(1)] * self.nl  # init grid
+        a = torch.tensor(anchors).float().view(self.nl, -1, 2)
+        self.register_buffer('anchors', a)  # shape(nl,na,2)
+        self.register_buffer('anchor_grid', a.clone().view(self.nl, 1, -1, 1, 1, 2))  # shape(nl,1,na,1,1,2)
+        self.m = nn.ModuleList(nn.Conv2d(x, self.no * self.na, 1) for x in ch)  # output conv
+        self.inplace = inplace  # use in-place ops (e.g. slice assignment)
 
-#     def forward(self, x):
-#         # x = x.copy()  # for profiling
-#         z = []  # inference output
-#         for i in range(self.nl):
-#             x[i] = self.m[i](x[i])  # conv
-#             bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
-#             x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
+    def forward(self, x):
+        # x = x.copy()  # for profiling
+        z = []  # inference output
+        for i in range(self.nl):
+            x[i] = self.m[i](x[i])  # conv
+            bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
+            x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
 
-#             if not self.training:  # inference
-#                 if self.grid[i].shape[2:4] != x[i].shape[2:4] or self.onnx_dynamic:
-#                     self.grid[i] = self._make_grid(nx, ny).to(x[i].device)
+            if not self.training:  # inference
+                if self.grid[i].shape[2:4] != x[i].shape[2:4] or self.onnx_dynamic:
+                    self.grid[i] = self._make_grid(nx, ny).to(x[i].device)
 
-#                 y = x[i].sigmoid()
-#                 if self.inplace:
-#                     y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
-#                     y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
-#                 else:  # for YOLOv5 on AWS Inferentia https://github.com/ultralytics/yolov5/pull/2953
-#                     xy = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
-#                     wh = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i].view(1, self.na, 1, 1, 2)  # wh
-#                     y = torch.cat((xy, wh, y[..., 4:]), -1)
-#                 z.append(y.view(bs, -1, self.no))
+                y = x[i].sigmoid()
+                if self.inplace:
+                    y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
+                    y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
+                else:  # for YOLOv5 on AWS Inferentia https://github.com/ultralytics/yolov5/pull/2953
+                    xy = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
+                    wh = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i].view(1, self.na, 1, 1, 2)  # wh
+                    y = torch.cat((xy, wh, y[..., 4:]), -1)
+                z.append(y.view(bs, -1, self.no))
 
-#         return x if self.training else (torch.cat(z, 1), x)
+        return x if self.training else (torch.cat(z, 1), x)
 
-#     @staticmethod
-#     def _make_grid(nx=20, ny=20):
-#         yv, xv = torch.meshgrid([torch.arange(ny), torch.arange(nx)])
-#         return torch.stack((xv, yv), 2).view((1, 1, ny, nx, 2)).float()
+    @staticmethod
+    def _make_grid(nx=20, ny=20):
+        yv, xv = torch.meshgrid([torch.arange(ny), torch.arange(nx)])
+        return torch.stack((xv, yv), 2).view((1, 1, ny, nx, 2)).float()
 
 
 # class Model(nn.Module):
