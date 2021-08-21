a60ac3459bf88045ebdc83c292d22e8a8e660ca0
vishalw-iitk <vishalwghmre88@gmail.com>
2021-08-22 02:11:34 +0530

    qat quant convet device cpu

--- Model_compression/Quantization/Pytorch/QAT/yolov5_repo/utils/loss.py
+++ Model_compression/Quantization/Pytorch/QAT/yolov5_repo/utils/loss.py
@@ -153,8 +153,8 @@ class ComputeLoss:
                 # with open('targets.txt', 'a') as file:
                 #     [file.write('%11.5g ' * 4 % tuple(x) + '\n') for x in torch.cat((txy[i], twh[i]), 1)]
 
-            if tobj.is_cuda == True and pi.is_cuda == False:
-                tobj = tobj.cpu()
+            if pi.is_cuda == True and tobj.is_cuda == False:
+                pi = pi.cpu()
             
             obji = self.BCEobj(pi[..., 4], tobj)
             lobj += obji * self.balance[i]  # obj loss
