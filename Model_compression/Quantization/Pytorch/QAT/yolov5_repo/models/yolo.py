3b1ac236eabe3cac0bd983c7e0f3bdc93b16daf0
vishalw-iitk <vishalwghmre88@gmail.com>
2021-08-22 01:14:33 +0530

    qat quant convet device cpu

--- Model_compression/Quantization/Pytorch/QAT/yolov5_repo/models/yolo.py
+++ Model_compression/Quantization/Pytorch/QAT/yolov5_repo/models/yolo.py
@@ -67,6 +67,9 @@ class Detect(nn.Module):
 
                 y = x[i].sigmoid()
                 if self.inplace:
+                    if self.stride[i].is_cuda == True and y[..., 0:2].is_cuda == False:
+                        self.stride[i] = self.stride[i].cpu()
+
                     y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
                     y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
                 else:  # for YOLOv5 on AWS Inferentia https://github.com/ultralytics/yolov5/pull/2953
