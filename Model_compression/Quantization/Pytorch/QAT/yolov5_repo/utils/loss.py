0f03bba3acb135cc3859f0855bded0785074fabf
vishalw-iitk <vishalwghmre88@gmail.com>
2021-08-22 20:31:53 +0530

    cuda QAT loss

--- Model_compression/Quantization/Pytorch/QAT/yolov5_repo/utils/loss.py
+++ Model_compression/Quantization/Pytorch/QAT/yolov5_repo/utils/loss.py
@@ -94,7 +94,7 @@ class ComputeLoss:
         # device = torch.device('cpu')
         h = model.hyp  # hyperparameters
         print("type(h", type(h))
-        print("still loss func", device, h['cls_pw'].is_cuda)
+        print("still loss func", device, model.device)
 
         # Define criteria
         BCEcls = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['cls_pw']], device=device))
