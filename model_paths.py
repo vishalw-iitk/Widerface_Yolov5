0f7818116745a7d162f8a07e8eb6e71147ffd66b
vishalw-iitk <vishalwghmre88@gmail.com>
2021-08-25 18:12:05 +0530

    Model paths code-cleaning. Half-done

--- model_paths.py
+++ model_paths.py
@@ -1,61 +1,70 @@
+'''
+This file stores the relative paths of every model.
+It is expected to maintain the RAW_DATASET and Pre_trained_model folders path wrt dts folder so as to run the code properly
+'''
+
+# Libraries
 import os
+
 def frameworks(skip_training, running_model_paths, pre_trained_model_paths):
+    '''
+    Stores the path of fp32 models of different frameworks. Required in the export model task.
+    ARGS :
+        skip_training : Updation based whether on pre-trained models are used or runs(training) models are used for export
+        running_model_paths : Stores the path of model on which inference could be done
+        pre_trained_model_paths : Stores the path of pre-trained-models
+    RETURNS :
+        framework_path : Updated framework paths as per the skip-training status
+    '''
+
     if skip_training == False: #training
         framework_path = {
             'Pytorch':{
                 'Regular_Pytorch': running_model_paths['Regular']['Pytorch']['fp32']
-                # 'Regular_fp16': running_model_paths['Regular']['Pytorch']['fp16']
-                # 'QAT quantized': 'dts', 'Model_compression', 'Quantization', 'Pytorch', 'QAT', 'runs', 'train', 'QAT', 'yolov5s_results6', 'weights', 'best.pt'
             },
             'ONNX':{
                 'Regular_Pytorch': os.path.join('..', 'runs', 'Regular', 'Regular_Pytorch', 'weights', 'fp32', 'best.onnx')
-                # '..', 'runs', 'Regular', 'Regular_Pytorch', 'weights', 'fp32', 'best.onnx'
-                # 'Quantized_tfl_fp16' : '..', 'runs', 'train', 'yolov5s_results', 'weights', 'fp32', 'best.onnx',
-                # 'Quantized_tfl_int8' : '..', 'runs', 'train', 'yolov5s_results', 'weights', 'fp32', 'best.onnx'
-                # 'QAT quantized': 'dts', 'Model_compression', 'Quantization', 'Pytorch', 'QAT', 'runs', 'train', 'QAT', 'yolov5s_results6', 'weights', 'best.onnx'
             },
             'tf_pb':{
                 'Regular_Pytorch': os.path.join('..', 'runs', 'Regular', 'Regular_Pytorch', 'weights', 'fp32', 'best.pb', 'saved_model.pb'),
                 'Quantized_Tflite_fp16' : os.path.join('..', 'runs', 'Regular', 'Regular_Pytorch', 'weights', 'fp32', 'best.pb', 'saved_model.pb'),
                 'Quantized_Tflite_int8' : os.path.join('..', 'runs', 'Regular', 'Regular_Pytorch', 'weights', 'fp32', 'best.pb', 'saved_model.pb')
-                # 'QAT quantized': 'dts', 'Model_compression', 'Quantization', 'Pytorch', 'QAT', 'runs', 'train', 'QAT', 'yolov5s_results6', 'weights', 'best.pb', 'saved_model.pb'
             },
             'tflite':{
                 'Regular_Pytorch': os.path.join('..', 'runs', 'Regular', 'Regular_Pytorch', 'weights', 'fp32', 'best.tflite'),
                 'Quantized_Tflite_fp16' : os.path.join('..', 'Model_compression', 'Quantization', 'Tflite', 'fp16', 'best.tflite'),
                 'Quantized_Tflite_int8' : os.path.join('..', 'Model_compression', 'Quantization', 'Tflite', 'int8', 'best.tflite')
-                # 'QAT quantized': 'dts', 'Model_compression', 'Quantization', 'Pytorch', 'QAT', 'runs', 'train', 'QAT', 'yolov5s_results6', 'weights', 'best.tflite'
             }
         }
-    else:
+    else: # skip-training : means paths are to be updated with pre-trained model paths
         framework_path = {
             'Pytorch':{
                 'Regular_Pytorch': pre_trained_model_paths['Regular']['Pytorch']['fp32']
-                # 'Regular_fp16': pre_trained_model_paths['Regular']['Pytorch']['fp16']
-                # 'QAT quantized': 'dts', 'Model_compression', 'Quantization', 'Pytorch', 'QAT', 'runs', 'train', 'QAT', 'yolov5s_results6', 'weights', 'best.pt'
             },
             'ONNX':{
                 'Regular_Pytorch': os.path.join('..', 'Pre_trained_model', 'Regular', 'Pytorch', 'fp32', 'best.onnx')
-                # 'Quantized_tfl_fp16' : '..', 'runs', 'train', 'yolov5s_results', 'weights', 'fp32', 'best.onnx',
-                # 'Quantized_tfl_int8' : '..', 'runs', 'train', 'yolov5s_results', 'weights', 'fp32', 'best.onnx'
-                # 'QAT quantized': 'dts', 'Model_compression', 'Quantization', 'Pytorch', 'QAT', 'runs', 'train', 'QAT', 'yolov5s_results6', 'weights', 'best.onnx'
             },
             'tf_pb':{
                 'Regular_Pytorch': os.path.join('..', 'Pre_trained_model', 'Regular', 'Pytorch', 'fp32', 'best.pb', 'saved_model.pb'),
                 'Quantized_Tflite_fp16' : os.path.join('..', 'Pre_trained_model', 'Regular', 'Pytorch', 'fp32', 'best.pb', 'saved_model.pb'),
                 'Quantized_Tflite_int8' : os.path.join('..', 'Pre_trained_model', 'Regular', 'Pytorch', 'fp32', 'best.pb', 'saved_model.pb')
-                # 'QAT quantized': 'dts', 'Model_compression', 'Quantization', 'Pytorch', 'QAT', 'runs', 'train', 'QAT', 'yolov5s_results6', 'weights', 'best.pb', 'saved_model.pb'
             },
             'tflite':{
                 'Regular_Pytorch': os.path.join('..', 'Pre_trained_model', 'Regular', 'Pytorch', 'fp32', 'best.tflite'),
                 'Quantized_Tflite_fp16' : os.path.join('..', 'Model_compression', 'Quantization', 'Tflite', 'fp16', 'best.tflite'),
                 'Quantized_Tflite_int8' : os.path.join('..', 'Model_compression', 'Quantization', 'Tflite', 'int8', 'best.tflite')
-                # 'QAT quantized': 'dts', 'Model_compression', 'Quantization', 'Pytorch', 'QAT', 'runs', 'train', 'QAT', 'yolov5s_results6', 'weights', 'best.tflite'
             }
         }
     return framework_path
 
 def running_model_dictionary():
+    '''
+    Stores the path of model on which inference could be done(training sometimes). It is maintained during the entire pipeline run.
+    It could be runs/(training/trained models) or pre-trained models
+    RETURNS :
+        running_model_paths : Stores the path of model on which inference could be done
+    '''
+
     running_model_paths = {
         'Regular': {
             'Pytorch' : {
@@ -65,7 +74,7 @@ def running_model_dictionary():
             'Tflite' : {
                 'fp32' : os.path.join('..', 'runs', 'Regular', 'Regular_Pytorch', 'weights', 'fp32', 'best.tflite') #'path-if-unclonde-getfp16',
             }
-        }, # fp32 if fp32 not None else fp16
+        },
         'Quantization': {
             'Pytorch' : {
                 'PTQ' : os.path.join('..', 'Model_compression', 'Quantization', 'Pytorch', 'PTQ', 'best.pt'), #'path',
@@ -92,6 +101,14 @@ def running_model_dictionary():
     return running_model_paths
 
 def pre_trained_model_dictionary():
+    '''
+    Stores the path of pre-trained model on which inference could be done(training sometimes).
+    Pre-model model paths are useful specially when training process takes a lot time
+    RETURNS :
+        pre_trained_model_paths : Stored the paths of pre-trained models.
+                                PTQ/exports are done anyhow quickly, so pre-trained for this same as the model \
+                                stored in Model_Compression generated folder
+    '''
     pre_trained_model_paths = {
         'Regular' : {
             'Pytorch' : {
@@ -119,19 +136,32 @@ def pre_trained_model_dictionary():
     }
     return pre_trained_model_paths
 
-def update_to_running_paths_with_pretrianed(running_model_paths, pre_trained_model_paths):
-    running_model_paths['Regular']['Pytorch']['fp32'] = pre_trained_model_paths['Regular']['Pytorch']['fp32']
-    running_model_paths['Regular']['Pytorch']['fp16'] = pre_trained_model_paths['Regular']['Pytorch']['fp16']
-    running_model_paths['Regular']['Tflite']['fp32'] = pre_trained_model_paths['Regular']['Tflite']['fp32']
+def update_to_running_paths_with_pretrianed(running_model_paths, pre_trained_model_paths, skip_train, skip_QAT_train, skip_Pruning, skip_P1_training, skip_P2_training, skip_P3_training, skip_P4_training):
+    if skip_train:
+        running_model_paths['Regular']['Pytorch']['fp32'] = pre_trained_model_paths['Regular']['Pytorch']['fp32']
+        running_model_paths['Regular']['Pytorch']['fp16'] = pre_trained_model_paths['Regular']['Pytorch']['fp16']
+        running_model_paths['Regular']['Tflite']['fp32'] = pre_trained_model_paths['Regular']['Tflite']['fp32']
     
-    running_model_paths['Quantization']['Pytorch']['QAT'] = pre_trained_model_paths['Quantization']['Pytorch']['QAT']
+    if skip_QAT_train:
+        running_model_paths['Quantization']['Pytorch']['QAT'] = pre_trained_model_paths['Quantization']['Pytorch']['QAT']
     
-    running_model_paths['Pruning']['Pytorch']['P1'] = pre_trained_model_paths['Pruning']['Pytorch']['P1']
-    running_model_paths['Pruning']['Pytorch']['P2'] = pre_trained_model_paths['Pruning']['Pytorch']['P2']
-    running_model_paths['Pruning']['Pytorch']['P3'] = pre_trained_model_paths['Pruning']['Pytorch']['P3']
-    running_model_paths['Pruning']['Pytorch']['P4'] = pre_trained_model_paths['Pruning']['Pytorch']['P4']
+    if skip_Pruning:
+        running_model_paths['Pruning']['Pytorch']['P1'] = pre_trained_model_paths['Pruning']['Pytorch']['P1']
+        running_model_paths['Pruning']['Pytorch']['P2'] = pre_trained_model_paths['Pruning']['Pytorch']['P2']
+        running_model_paths['Pruning']['Pytorch']['P3'] = pre_trained_model_paths['Pruning']['Pytorch']['P3']
+        running_model_paths['Pruning']['Pytorch']['P4'] = pre_trained_model_paths['Pruning']['Pytorch']['P4']
+    else:
+        if skip_P1_training:
+            running_model_paths['Pruning']['Pytorch']['P1'] = pre_trained_model_paths['Pruning']['Pytorch']['P1']
+        if skip_P2_training:
+            running_model_paths['Pruning']['Pytorch']['P2'] = pre_trained_model_paths['Pruning']['Pytorch']['P2']
+        if skip_P3_training:
+            running_model_paths['Pruning']['Pytorch']['P3'] = pre_trained_model_paths['Pruning']['Pytorch']['P3']
+        if skip_P4_training:
+            running_model_paths['Pruning']['Pytorch']['P4'] = pre_trained_model_paths['Pruning']['Pytorch']['P4']
 
     return running_model_paths
+
 
 def prune_with_pre_trained_only(running_model_paths, pre_trained_model_paths):
     running_model_paths['Pruning']['Pytorch']['P1'] = pre_trained_model_paths['Pruning']['Pytorch']['P1']
