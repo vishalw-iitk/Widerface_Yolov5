4c09129ce510133f5f630e70798ffdd66212af66
Geethasree MN <mngeethasree97@gmail.com>
2021-08-30 19:07:09 +0530

    pruning parsers cleaned

--- Model_compression/Pruning/pruning.py
+++ Model_compression/Pruning/pruning.py
@@ -5,7 +5,7 @@ from dts.Model_compression.Pruning.Pytorch import prune_train
 from dts.Model_compression.Pruning.Pytorch.P4 import train
 from dts.model_paths import train_results_dictionary
 from dts.model_paths import model_defined_names
-from dts.model_paths import pre_trained_model_dictionary
+from dts.model_paths import running_model_dictionary
 
 
 class Pruning(object):
@@ -54,7 +54,7 @@ def main(opt):
     
     train_results_paths = train_results_dictionary()
     model_names = model_defined_names()
-    pre_trained_model_paths = pre_trained_model_dictionary()
+    running_model_paths = running_model_dictionary()
 
     '''
     P1 Pruning : Pruning on previously trained weights with Random Reinitialize.
@@ -67,12 +67,12 @@ def main(opt):
         p1_py = P1()
         for i in range(opt.num_iterations):
             print("++++++    Starting iteration " + str(i) + " of P1 iterative pruning    +++++++++")
-            p1_py.prune(weights=opt.weights if i==0 else train_results_paths['Pruning']['Pytorch']['P1']+'/'+model_names['Pruning']['Pytorch']['P1']+'/weights/best.pt',
-                        batch_size=opt.batch_size, imgsz=opt.img,epochs=opt.prune_retrain_epochs,
+            p1_py.prune(weights=opt.pre_trained_model_paths['Regular']['Pytorch']['fp32'] if i==0 else running_model_paths['Pruning']['Pytorch']['P1'],
+                        batch_size=opt.batch_size, imgsz=opt.img_size,epochs=opt.prune_retrain_epochs,
                         project = train_results_paths['Pruning']['Pytorch']['P1'],
                         name=model_names['Pruning']['Pytorch']['P1'],     
                         prune_perc=opt.prune_perc,prune_iter=i,
-                        random_reinit=False, theta0_reinit=False,
+                        random_reinit=True, theta0_reinit=False,
                         data=opt.data,cfg=opt.cfg, 
                         hyp=opt.hyp,exist_ok=True,cache_images=True,device=opt.device)
 
@@ -82,14 +82,14 @@ def main(opt):
         p2_py = P2()
         for i in range(opt.num_iterations):
             print("++++++    Starting iteration " + str(i) + " of P2 iterative pruning    +++++++++")
-            p2_py.prune(weights=opt.weights if i==0 else train_results_paths['Pruning']['Pytorch']['P2']+'/'+model_names['Pruning']['Pytorch']['P2']+'/weights/best.pt', 
-                    batch_size=opt.batch_size, imgsz=opt.img,epochs=opt.prune_retrain_epochs,
+            p2_py.prune(weights=opt.pre_trained_model_paths['Regular']['Pytorch']['fp32'] if i==0 else running_model_paths['Pruning']['Pytorch']['P2'], 
+                    batch_size=opt.batch_size, imgsz=opt.img_size,epochs=opt.prune_retrain_epochs,
                     project = train_results_paths['Pruning']['Pytorch']['P2'],
                     name=model_names['Pruning']['Pytorch']['P2'],     
                     prune_perc=opt.prune_perc,prune_iter=i,
-                    random_reinit=False, theta0_reinit=True,theta0_weights=pre_trained_model_paths['Pruning']['Pytorch']['theta0'],
+                    random_reinit=False, theta0_reinit=True,theta0_weights=opt.pre_trained_model_paths['Pruning']['Pytorch']['theta0'],
                     data=opt.data,cfg=opt.cfg, 
-                    hyp=opt.hyp,exist_ok=True)
+                    hyp=opt.hyp,exist_ok=True,cache_images=True,device=opt.device)
 
     if opt.skip_P4_training == False:
         p4_py = P4()
@@ -111,41 +111,19 @@ def main(opt):
 
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--skip-train', action='store_true', help='skip the time taking regular training')
     parser.add_argument('--weights', type=str, default='yolov5s.pt', help='initial weights path')
     parser.add_argument('--cfg', type=str, default='', help='model.yaml path')
     parser.add_argument('--data', type=str, default='data/coco128.yaml', help='dataset.yaml path')
     parser.add_argument('--hyp', type=str, default='data/hyps/hyp.scratch.yaml', help='hyperparameters path')
-    parser.add_argument('--epochs', type=int, default=300)
+    parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs')
-    parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='train, val image size (pixels)')
-    parser.add_argument('--rect', action='store_true', help='rectangular training')
-    parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
-    parser.add_argument('--nosave', action='store_true', help='only save final checkpoint')
-    parser.add_argument('--noval', action='store_true', help='only validate final epoch')
-    parser.add_argument('--noautoanchor', action='store_true', help='disable autoanchor check')
-    parser.add_argument('--evolve', type=int, nargs='?', const=300, help='evolve hyperparameters for x generations')
-    parser.add_argument('--bucket', type=str, default='', help='gsutil bucket')
+    parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=416, help='train, val image size (pixels)')
     parser.add_argument('--cache-images', action='store_true', help='cache images for faster training')
-    parser.add_argument('--image-weights', action='store_true', help='use weighted image selection for training')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--multi-scale', action='store_true', help='vary img-size +/- 50%%')
     parser.add_argument('--single-cls', action='store_true', help='train multi-class data as single-class')
     parser.add_argument('--adam', action='store_true', help='use torch.optim.Adam() optimizer')
-    parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode')
     parser.add_argument('--workers', type=int, default=8, help='maximum number of dataloader workers')
-    parser.add_argument('--qat-project', default='../runs_QAT/train', help='save to project/name')
-    parser.add_argument('--entity', default=None, help='W&B entity')
-    parser.add_argument('--qat-name', default='exp', help='save to project/name')
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--quad', action='store_true', help='quad dataloader')
-    parser.add_argument('--linear-lr', action='store_true', help='linear LR')
-    parser.add_argument('--label-smoothing', type=float, default=0.0, help='Label smoothing epsilon')
-    parser.add_argument('--upload_dataset', action='store_true', help='Upload dataset as W&B artifact table')
-    parser.add_argument('--bbox_interval', type=int, default=-1, help='Set bounding-box image logging interval for W&B')
-    parser.add_argument('--save_period', type=int, default=-1, help='Log model after every "save_period" epoch')
-    parser.add_argument('--artifact_alias', type=str, default="latest", help='version of dataset artifact to be used')
-    parser.add_argument('--local_rank', type=int, default=-1, help='DDP parameter, do not modify')
     opt = parser.parse_known_args()[0] if known else parser.parse_args()
     return opt
 
